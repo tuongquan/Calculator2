2ed92eb3fdc27a89904514fe100800afb73ce276
tuongquan <69913836+tuongquan@users.noreply.github.com>
2022-03-17 17:56:09 +0700

    xu ly giao dien

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -29,13 +29,123 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtCong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtCong
+            // 
+            this.BtCong.BackColor = System.Drawing.Color.DarkGray;
+            this.BtCong.ForeColor = System.Drawing.Color.Red;
+            this.BtCong.Location = new System.Drawing.Point(154, 274);
+            this.BtCong.Name = "BtCong";
+            this.BtCong.Size = new System.Drawing.Size(127, 71);
+            this.BtCong.TabIndex = 0;
+            this.BtCong.Text = "+";
+            this.BtCong.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(174, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tinh Toan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(56, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(56, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "B";
+            // 
+            // txtA
+            // 
+            this.txtA.ForeColor = System.Drawing.Color.Blue;
+            this.txtA.Location = new System.Drawing.Point(132, 95);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(218, 32);
+            this.txtA.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            this.txtB.ForeColor = System.Drawing.Color.Blue;
+            this.txtB.Location = new System.Drawing.Point(132, 144);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(218, 32);
+            this.txtB.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(26, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 26);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ket qua";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.ForeColor = System.Drawing.Color.Blue;
+            this.txtKQ.Location = new System.Drawing.Point(132, 197);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(218, 32);
+            this.txtKQ.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(465, 378);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtCong);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtCong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKQ;
     }
 }
 
